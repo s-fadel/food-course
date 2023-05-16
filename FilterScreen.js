2aff71bc34e649080fbc0aee270c36366fc547e6
stephanie <stephanie.fadel@hotmail.com>
2023-05-16 13:44:44 +0200

    Styling and design filter

--- FilterScreen.js
+++ FilterScreen.js
@@ -3,15 +3,65 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const FilterScreen = ({ setShowFilter }) => {
+  const [selectedOption, setSelectedOption] = React.useState(null);
+
+  const handleOptionPress = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.headingFilterMenu}>FILTER RECIPE</Text>
-        <TouchableOpacity
-          onPress={() => setShowFilter(false)}
-          style={styles.closeButton}
-        >
+        <TouchableOpacity onPress={() => setShowFilter(false)} style={styles.closeButton}>
           <Icon name="times" size={25} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.optionContainer}>
+        <TouchableOpacity
+          onPress={() => handleOptionPress("Förrätter")}
+          style={[
+            styles.optionButton,
+            selectedOption === "Förrätter",
+          ]}
+        >
+          {selectedOption === "Förrätter" ? (
+            <View style={styles.circleSelected} />
+          ) : (
+            <View style={styles.circle} />
+          )}
+          <Text style={styles.optionText}>FÖRRÄTTER</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleOptionPress("Huvudrätt")}
+          style={[
+            styles.optionButton,
+            selectedOption === "Huvudrätt",
+          ]}
+        >
+          {selectedOption === "Huvudrätt" ? (
+            <View style={styles.circleSelected} />
+          ) : (
+            <View style={styles.circle} />
+          )}
+          <Text style={styles.optionText}>HUVUDRÄTTER</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleOptionPress("Dessert")}
+          style={[
+            styles.optionButton,
+            selectedOption === "Dessert",
+          ]}
+        >
+          {selectedOption === "Dessert" ? (
+            <View style={styles.circleSelected} />
+          ) : (
+            <View style={styles.circle} />
+          )}
+          <Text style={styles.optionText}>DESSERT</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -31,10 +81,43 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
   headingFilterMenu: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: "bold",
   },
   closeButton: {},
+  optionContainer: {
+    marginTop: 20,
+    paddingHorizontal: 30,
+  },
+  optionButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    marginTop: 30,
+  },
+  selectedOption: {
+    marginTop: 10,
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "black",
+    marginRight: 10,
+  },
+  circleSelected: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: "black",
+    marginRight: 10,
+  },
+  optionText: {
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: "600",
+  },
 });
 
 export default FilterScreen;
